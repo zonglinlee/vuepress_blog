8eeb4c7d4ac6468dc03f39eb7d6d18dd5ec82e79
zonglinlee <173389705@qq.com>
2022-05-10 17:28:37 +0800

    增加Google英文字体

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,6 +12,7 @@ module.exports = {
   // 自定义header
   head: [
       ['link', { rel: 'icon', href: '/images/favicon_io/favicon-32x32.png' }],
+      // ['link', { rel: 'stylesheet', href: 'https://fonts.googlefonts.cn/css?family=Merriweather' }],
       ['script', {  src: myBase + 'js/tailwind.css.js' }]
   ],
   themeConfig: {
