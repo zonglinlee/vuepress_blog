c48cd0b44a8760b1cdf2d7e4c4cf52079869c37e
lizonglin <173389705@qq.com>
2024-01-11 13:38:36 +0800

    add

--- docs/_code/wechat/wechat_sdk_node/wechatSetting.js
+++ docs/_code/wechat/wechat_sdk_node/wechatSetting.js
@@ -1,5 +1,5 @@
-const appId = 'wxab39f38b5b26875b' // 测试公众号的addId
-const appSecret = '3d6a521664c695dfcbb86f404cfd7dda' // 测试公众号的appSecret
+const appId = 'wx60a9282346f4e3c7' // 测试公众号的addId
+const appSecret = 'c85fc01b1c34c4e6f8fc9b9b202460' // 测试公众号的appSecret
 module.exports = {
   appId,
   appSecret,
